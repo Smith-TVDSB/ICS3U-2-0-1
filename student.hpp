d21a8031d38872ad524d1393ca418221e9a1da9a
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2021-10-05 19:22:18 -0400

    Initial commit

---
+++ student.hpp
@@ -0,0 +1,31 @@
+#pragma once // prevents multiple definitions
+
+#include <iostream> // print library
+#include <string>
+using namespace std;
+
+// ===============
+// implementations
+
+int add(int a, int b)
+{
+	return 0; // TODO: fix
+}
+
+// Exercise 2: implements function that calculates remainder
+int remainder(int a, int b)
+{
+	return 0; // TODO: fix
+}
+
+//Exercise 3: implements function that calculates circumference
+float circumference(float r)
+{
+	return 0; //TODO: fix
+}
+
+//Exercise 4: concate first name, middle initial, and last name in proper format
+string concat(string firstName, char middleInitial, string lastName)
+{
+	return "0";//TODO: fix
+}
